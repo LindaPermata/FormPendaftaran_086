5a83157bb9e473aaa70f612d487aad07b343cc2c
LindaPermata <permatalinda531@gmail.com>
2025-10-31 17:02:13 +0700

    Menambahkan spacer untuk memberi jarak

--- app/src/main/java/com/example/formpendaftaran/Form.kt
+++ app/src/main/java/com/example/formpendaftaran/Form.kt
@@ -112,6 +112,8 @@ fun FormulirPendaftaran(modifier: Modifier = Modifier) {
                         Text(item)
                     }
                 }
+
+                Spacer(modifier = Modifier.height(16.dp))
         }
 
 
