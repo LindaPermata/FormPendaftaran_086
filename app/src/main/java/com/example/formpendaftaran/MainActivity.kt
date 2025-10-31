91b8ed169fc46791f7e92df8e332b600cfe6f4bc
LindaPermata <permatalinda531@gmail.com>
2025-10-31 17:06:54 +0700

    Mengubah code di MainActivity agar bisa dijalankan

--- app/src/main/java/com/example/formpendaftaran/MainActivity.kt
+++ app/src/main/java/com/example/formpendaftaran/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             FormPendaftaranTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FormulirPendaftaran(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
